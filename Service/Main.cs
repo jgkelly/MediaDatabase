b4cf9d57406e6632975f0f22e0cf2f1c2e30b267
Jeff Kelly <jgkelly1022@gmail.com>
2012-08-23 22:39:55 -0400

    Solution Created and Added to repository

---
+++ Service/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Service
+{
+   public partial class Main : ServiceBase
+   {
+      public Main()
+      {
+         InitializeComponent();
+      }
+
+      protected override void OnStart(string[] args)
+      {
+      }
+
+      protected override void OnStop()
+      {
+      }
+   }
+}
